298c07ea3150952102d65d49df2d76b98c211509
karananeja <karananeja.r@gmail.com>
2022-11-13 12:13:46 +0530

    added the home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import './App.css';
+import Home from './pages/Home.js';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <Home />
+    </div>
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,19 @@
+import { East } from '@mui/icons-material';
 import React from 'react';
+import '../css/Home.css';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='home__layout'>
+        <div className='home__layout__left'></div>
+        <div className='home__layout__right'>
+          <h1>Lorem ipsum dolor</h1>
+          <p>Lorem ipsum dolorsit amet consectetur, adipisicing elit. Excepturi sit eaque, exercitationem praesentium ad nulla quasi ratione distinctio repellat, omnis debitis veritatis neque dolorem officia ut aliquip ex ea commodo consequat.</p>
+          <button className='home__layout__btn'>Select Module <East /></button>
+        </div>
+      </div>
+    </div>
   );
 };
 
