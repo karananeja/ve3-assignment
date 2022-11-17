cd06fd7f41eb2972278a9f3d2460e9bd97b24af1
karananeja <karananeja.r@gmail.com>
2022-11-17 20:47:53 +0530

    updated the module component

--- src/modules/Module.js
+++ src/modules/Module.js
@@ -1,12 +1,14 @@
-import { MoreVert, Search } from '@mui/icons-material';
+import { MoreVert, Search as SearchIcon } from '@mui/icons-material';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import React from 'react';
+import React, { useState } from 'react';
 import moduleContent from '../moduleContent.json';
 import '../css/Module.css';
 import { useStateValue } from '../StateProvider';
+import Search from '../pages/Search';
 
 const Module = () => {
   const [{ moduleNumber }] = useStateValue();
+  const [isSearch, setIsSearch] = useState(false);
 
   const handleClick = (e) => {
     e.preventDefault();
@@ -14,44 +16,54 @@ const Module = () => {
   };
 
   return (
-    <div className='module'>
-      <div className='module__layout'>
-        <div className='module__header'>
-          <h3 className='module__heading'>{moduleNumber}</h3>
-          <div className='module__icons'>
-            <Search />
-            <MoreVert onClick={(e) => handleClick(e)} />
-          </div>
-        </div>
-        <div className='module__tabContainer'>
-          {
-            moduleContent.map((module, index) => (
-              <Tabs className='module__tabs' key={index}>
-                <TabList>
-                  {
-                    module[moduleNumber].tabsInfo.tabs.map((tab, tabIndex) => (
-                      <Tab key={tabIndex}>
-                        {tab}
-                      </Tab>
-                    ))
-                  }
-                </TabList>
+    <>
+      {!isSearch ?
+        (
+          <div className='module'>
+            <div className='module__layout'>
+              <div className='module__header'>
+                <h3 className='module__heading'>{moduleNumber}</h3>
+                <div className='module__icons'>
+                  <SearchIcon onClick={() => setIsSearch(true)} />
+                  <MoreVert onClick={(e) => handleClick(e)} />
+                </div>
+              </div>
+              <div className='module__tabContainer'>
                 {
-                  module[moduleNumber].tabsInfo.tabParaGraphs.map((content, contentIndex) => (
-                    <TabPanel className='module__tabPanel' key={contentIndex}>
-                      <div className='module__tabImage' style={{ background: `url(${module[moduleNumber].tabsInfo.tabImages[contentIndex]}) no-repeat` }}></div>
-                      <div className='module__tab'>
-                        <h3>{module[moduleNumber].tabsInfo.tabHeadings[contentIndex]}</h3>
-                        <p>{content}</p>
-                      </div>
-                    </TabPanel>
-                  ))
-                }
-              </Tabs>
-            ))}
-        </div>
-      </div>
-    </div >
+                  moduleContent.map((module, index) => (
+                    <Tabs className='module__tabs' key={index}>
+                      <TabList>
+                        {
+                          module[moduleNumber].tabsInfo.tabs.map((tab, tabIndex) => (
+                            <Tab key={tabIndex}>
+                              {tab}
+                            </Tab>
+                          ))
+                        }
+                      </TabList>
+                      {
+                        module[moduleNumber].tabsInfo.tabParaGraphs.map((content, contentIndex) => (
+                          <TabPanel className='module__tabPanel' key={contentIndex}>
+                            <div className='module__tabImage' style={{ background: `url(${module[moduleNumber].tabsInfo.tabImages[contentIndex]}) no-repeat` }}></div>
+                            <div className='module__tab'>
+                              <h3>{module[moduleNumber].tabsInfo.tabHeadings[contentIndex]}</h3>
+                              <p>{content}</p>
+                            </div>
+                          </TabPanel>
+                        ))
+                      }
+                    </Tabs>
+                  ))}
+              </div>
+            </div>
+          </div >
+        )
+        :
+        (
+          <Search />
+        )
+      }
+    </>
   );
 };
 
