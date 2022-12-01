e3a0d7b10ab133891a0cae575c9ef8c5a986f718
karananeja <karananeja.r@gmail.com>
2022-12-01 07:50:36 +0530

    implemented search functionality

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,30 +1,26 @@
 import { MoreVert, Search as SearchIcon } from '@mui/icons-material';
 import React, { useState } from 'react';
 import '../css/Search.css';
-import moduleContent from '../moduleContent.json';
+import modifiedModuleContent from '../modifiedModuleContent.json';
 
 const Search = () => {
   const [search, setSearch] = useState('');
-  const [headings, setHeadings] = useState([]);
-  const [paragraphs, setParagraphs] = useState([]);
+  const [searchContent, setSearchContent] = useState([]);
 
   const moduleNames = ['Module One', 'Module Two', 'Module Three', 'Module Four', 'Module Five'];
 
   const handleSearch = () => {
-    moduleContent.map(module => (
+    setSearchContent([]);
+    modifiedModuleContent.map(module => (
       moduleNames.map(name => {
-        module[name].tabsInfo.tabHeadings.map(headingList => {
-          if (headingList.toLowerCase().includes(search) && search.length !== 0) setHeadings(arr => [...arr, headingList]);
+        Object.keys(module[name].tabsInfo).forEach(tab => {
+          if (module[name].tabsInfo[tab].heading.toLowerCase().includes(search) && module[name].tabsInfo[tab].paragraph.toLowerCase().includes(search) && search.length > 0) setSearchContent(content => [...content, module[name].tabsInfo[tab]]);
         });
 
-        module[name].tabsInfo.tabParaGraphs.map(paragraphList => {
-          if (paragraphList.toLowerCase().includes(search) && search.length !== 0) setParagraphs(arr => [...arr, paragraphList]);
-        });
+        return searchContent;
       })
     ));
   };
-
-  console.log(headings, paragraphs);
 
   const handleClick = (e) => {
     e.preventDefault();
@@ -45,7 +41,22 @@ const Search = () => {
             <input className='search__input' type="text" placeholder='type text and press enter...' value={search} onChange={(e) => setSearch(e.target.value)} />
             <SearchIcon onClick={handleSearch} />
           </div>
-          <p>Showing {headings.length > 0 && paragraphs.length > 0 ? paragraphs.length > headings.length ? `${paragraphs.length}` : `${headings.length}` : '0'} results...</p>
+          <p>Showing {searchContent.length > 0 ? `${searchContent.length}` : '0'} results...</p>
+          {
+            searchContent.length > 0 && searchContent.map((content, index) => (
+              <div className='search__result' key={index}>
+                <div className='search__result__left' style={{ background: `url(${content.image}) 0 0/contain no-repeat` }}></div>
+                <div className='search__result__right'>
+                  <h3>
+                    {content.heading}
+                  </h3>
+                  <p>
+                    {content.paragraph}
+                  </p>
+                </div>
+              </div>
+            ))
+          }
         </div>
       </div>
     </div>
